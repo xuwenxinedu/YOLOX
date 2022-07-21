566e9984ea01fcda50ec06078477eec3b3583131
xuwenxinedu <xuwenxin_edu@163.com>
2022-07-21 19:28:05 +0800

    change max epoch in yolox_voc_s

--- exps/example/yolox_voc/yolox_voc_s.py
+++ exps/example/yolox_voc/yolox_voc_s.py
@@ -12,6 +12,7 @@ class Exp(MyExp):
     def __init__(self):
         super(Exp, self).__init__()
         self.num_classes = 2
+        self.max_epoch = 20
         self.depth = 0.33
         self.width = 0.50
         self.warmup_epochs = 1

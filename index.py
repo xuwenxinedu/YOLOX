6532ca02e0a16517ffca978768babb6a564b54d8
xuwenxinedu <xuwenxin_edu@163.com>
2022-07-23 14:59:11 +0800

    the file index.py can work now

--- index.py
+++ index.py
@@ -3,87 +3,36 @@
 # Copyright (c) Megvii, Inc. and its affiliates.
 import sys
 sys.path.append('.')
-import argparse
+
 import os
 import time
 from loguru import logger
 import pandas as pd
 import cv2
-
+import numpy as np
 import torch
 
 from yolox.data.data_augment import ValTransform
 from yolox.data.datasets import COCO_CLASSES
 from yolox.exp import get_exp
-from yolox.utils import fuse_model, get_model_info, postprocess, vis
+from yolox.utils import fuse_model, get_model_info, postprocess
 
 IMAGE_EXT = [".jpg", ".jpeg", ".webp", ".bmp", ".png"]
 
-
-def make_parser():
-    parser = argparse.ArgumentParser("YOLOX Demo!")
-    
-    parser.add_argument("-expn", "--experiment-name", type=str, default=None)
-    parser.add_argument("-n", "--name", type=str, default=None, help="model name")
-
-    parser.add_argument(
-        "--path", default="./assets/dog.jpg", help="path to images or video"
-    )
-    parser.add_argument("--camid", type=int, default=0, help="webcam demo camera id")
-    parser.add_argument(
-        "--save_result",
-        action="store_true",
-        help="whether to save the inference result of image/video",
-    )
-
-    # exp file
-    parser.add_argument(
-        "-f",
-        "--exp_file",
-        default="./exps/default/yolox_s.py",
-        type=str,
-        help="please input your experiment description file",
-    )
-    parser.add_argument("-c", "--ckpt", default="best.pth", type=str, help="ckpt for eval")
-    parser.add_argument(
-        "--device",
-        default="gpu",
-        type=str,
-        help="device to run our model, can either be cpu or gpu",
-    )
-    parser.add_argument("--conf", default=0.3, type=float, help="test conf")
-    parser.add_argument("--nms", default=0.3, type=float, help="test nms threshold")
-    parser.add_argument("--tsize", default=None, type=int, help="test img size")
-    parser.add_argument(
-        "--fp16",
-        dest="fp16",
-        default=False,
-        action="store_true",
-        help="Adopting mix precision evaluating.",
-    )
-    parser.add_argument(
-        "--legacy",
-        dest="legacy",
-        default=False,
-        action="store_true",
-        help="To be compatible with older versions",
-    )
-    parser.add_argument(
-        "--fuse",
-        dest="fuse",
-        default=False,
-        action="store_true",
-        help="Fuse conv and bn for testing.",
-    )
-    parser.add_argument(
-        "--trt",
-        dest="trt",
-        default=False,
-        action="store_true",
-        help="Using TensorRT model for testing.",
-    )
-    return parser
-
+def viss(img, boxes, scores, cls_ids, conf=0.5, class_names=None, img_id = None):
+    objs = []
+    for i in range(len(boxes)):
+        box = boxes[i]
+        cls_id = int(cls_ids[i])
+        score = scores[i]
+        if score < conf:
+            continue
+        
+        objs.append(
+            [class_names[cls_id], img_id, str(float(score)), str(float(box[0])),
+             str(float(box[1])), str(float(box[2])), str(float(box[3]))]
+        )
+    return objs
 
 def get_image_list(path):
     image_names = []
@@ -164,11 +113,11 @@ class Predictor(object):
             logger.info("Infer time: {:.4f}s".format(time.time() - t0))
         return outputs, img_info
 
-    def visual(self, output, img_info, cls_conf=0.35):
+    def visual(self, output, img_info, cls_conf=0.35, img_id = ''):
         ratio = img_info["ratio"]
         img = img_info["raw_img"]
         if output is None:
-            return img
+            return []
         output = output.cpu()
 
         bboxes = output[:, 0:4]
@@ -179,114 +128,19 @@ class Predictor(object):
         cls = output[:, 6]
         scores = output[:, 4] * output[:, 5]
 
-        vis_res, objs = vis(img, bboxes, scores, cls, cls_conf, self.cls_names)
-        return vis_res, objs
-
-
-def image_demo(predictor, vis_folder, path, current_time, save_result):
-    if os.path.isdir(path):
-        files = get_image_list(path)
-    else:
-        files = [path]
-    files.sort()
-    for image_name in files:
-        outputs, img_info = predictor.inference(image_name)
-        result_image, objs = predictor.visual(outputs[0], img_info, predictor.confthre)
-        if save_result:
-            save_folder = os.path.join(
-                vis_folder, time.strftime("%Y_%m_%d_%H_%M_%S", current_time)
-            )
-            os.makedirs(save_folder, exist_ok=True)
-            save_file_name = os.path.join(save_folder, os.path.basename(image_name))
-            logger.info("Saving detection result in {}".format(save_file_name))
-            cv2.imwrite(save_file_name, result_image)
-        # ch = cv2.waitKey(0)
-        # if ch == 27 or ch == ord("q") or ch == ord("Q"):
-        #     break
-
-
-def main(exp, args):
-    if not args.experiment_name:
-        args.experiment_name = exp.exp_name
-
-    file_name = os.path.join(exp.output_dir, args.experiment_name)
-    os.makedirs(file_name, exist_ok=True)
-
-    vis_folder = None
-    if args.save_result:
-        vis_folder = os.path.join(file_name, "vis_res")
-        os.makedirs(vis_folder, exist_ok=True)
-
-    if args.trt:
-        args.device = "gpu"
-
-    logger.info("Args: {}".format(args))
-
-    if args.conf is not None:
-        exp.test_conf = args.conf
-    if args.nms is not None:
-        exp.nmsthre = args.nms
-    if args.tsize is not None:
-        exp.test_size = (args.tsize, args.tsize)
-
-    model = exp.get_model()
-    logger.info("Model Summary: {}".format(get_model_info(model, exp.test_size)))
-
-    if args.device == "gpu":
-        model.cuda()
-        if args.fp16:
-            model.half()  # to FP16
-    model.eval()
-
-    if not args.trt:
-        if args.ckpt is None:
-            ckpt_file = os.path.join(file_name, "best_ckpt.pth")
-        else:
-            ckpt_file = args.ckpt
-        logger.info("loading checkpoint")
-        ckpt = torch.load(ckpt_file, map_location="cpu")
-        # load the model state dict
-        model.load_state_dict(ckpt["model"])
-        logger.info("loaded checkpoint done.")
-
-    if args.fuse:
-        logger.info("\tFusing model...")
-        model = fuse_model(model)
-
-    if args.trt:
-        assert not args.fuse, "TensorRT model is not support model fusing!"
-        trt_file = os.path.join(file_name, "model_trt.pth")
-        assert os.path.exists(
-            trt_file
-        ), "TensorRT model is not found!\n Run python3 tools/trt.py first!"
-        model.head.decode_in_inference = False
-        decoder = model.head.decode_outputs
-        logger.info("Using TensorRT to inference")
-    else:
-        trt_file = None
-        decoder = None
-
-    predictor = Predictor(
-        model, exp, COCO_CLASSES, trt_file, decoder,
-        args.device, args.fp16, args.legacy,
-    )
-    current_time = time.localtime()
-    if True:
-        image_demo(predictor, vis_folder, args.path, current_time, args.save_result)
-    
+        objs = viss(img, bboxes, scores, cls, cls_conf, self.cls_names, img_id)
+        return objs
 
 
 def get_pred(exp):
-    logger.info('=========get pred==============')
+    
     exp.test_conf = 0.3
     exp.nmsthre = 0.3
     model = exp.get_model()
-    logger.info('=========get pred chpt 1==============')
     model.cuda()
     model.eval()
         
     ckpt_file = './best.pth'
-    logger.info("loading checkpoint")
     ckpt = torch.load(ckpt_file, map_location="cpu")
     # load the model state dict
     model.load_state_dict(ckpt["model"])
@@ -299,16 +153,17 @@ def get_pred(exp):
         model, exp, COCO_CLASSES, trt_file, decoder,
         'gpu', False, False,
     )
-    logger.info('=========get pred chpt 2==============')
 
     return predictor
     
 def write_csv(objs):
-    df = pd.DataFrame(columns=["label", "img_id", "confidence",
+    logger.info(f'objs {len(objs)} x {len(objs[0])}')
+    logger.info(objs)
+    df = pd.DataFrame(data=objs, columns=["label", "img_id", "confidence",
                         "xmin", "ymin", "xmax", "ymax"])
-    for obj in objs:
-        df = df.append(obj, ignore_index=True)
+    
     df.to_csv("/home/mw/project/results.csv", index=False)
+    logger.info(df)
     return df
 
 def my_image_demo(predictor, path):
@@ -320,20 +175,15 @@ def my_image_demo(predictor, path):
     all_objs = []
     for image_name in files:
         outputs, img_info = predictor.inference(image_name)
-        result_image, objs = predictor.visual(outputs[0], img_info, predictor.confthre)
         img_id = os.path.basename(image_name)[:-4]
+        objs = predictor.visual(outputs[0], img_info, predictor.confthre, img_id)
+        logger.info(objs)
         if len(objs) > 0:
             for i, obj in enumerate(objs):
-                objs[i]['img_id'] = img_id
                 all_objs.append(objs[i])
         else:
-            all_objs.append({'img_id':img_id})
-        
-        # ch = cv2.waitKey(0)
-        # if ch == 27 or ch == ord("q") or ch == ord("Q"):
-        #     break
-    logger.info('============below are all objects============')
-    print(all_objs)
+            all_objs.append(['', img_id, '','','','',''])
+    
     return write_csv(all_objs)
 
 def vid2img(file_path) :
@@ -341,7 +191,13 @@ def vid2img(file_path) :
         os.makedirs('./ddirs')
     for file in os.listdir(file_path):
         path = os.path.join(file_path, file)
+        if path[-4:] != '.mp4':
+            continue
+        logger.info(f'the path is {path}')
         vidcap = cv2.VideoCapture(path)
+        logger.info(vidcap.isOpened())
+        if not vidcap.isOpened():
+            return False
         success,image1 = vidcap.read()
         image2 = image1
         while success:
@@ -349,19 +205,22 @@ def vid2img(file_path) :
             success, image1 = vidcap.read()
         pic_name = os.path.basename(path)[:-4] + ".jpg"
         c = os.path.join("./ddirs/", pic_name)
-        print(c)
         cv2.imwrite(c, image2)
+    return True
 
 def invoke(_input:str) :
-    vid2img(_input)
-    # args = make_parser().parse_args()
-    # print(args)
+    res = vid2img(_input)
+    if not res:
+        logger.info('vid2img failed')
+        return
     exp = get_exp('./exps/default/yolox_s.py')
-    logger.info('predictor blow')
+    
     predictor = get_pred(exp)
-    logger.info('=========my image demo starting============')
-    df = my_image_demo(predictor, "./ddirs/")
-    print(df)
+    
+    my_image_demo(predictor, "./ddirs/")
+
+    return
+    
 
 if __name__ == "__main__":
     invoke('/gdrive/My Drive/yolo/videos')
